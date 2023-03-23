d512193178c4f324bb9cb3b08f994d51580d94ff
Hamad Nabizada <50067061+HamadNabizada@users.noreply.github.com>
2023-03-23 00:32:47 +0000

    update

--- q1.hpp
+++ q1.hpp
@@ -2,10 +2,21 @@
 #include <iomanip>
 using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void getinput(int &n1, int &n2){
+    cin >> n1 >> n2;
+}
+void getinput(float &n1, float &n2){
+    cin >> n1 >> n2;
+}
+
+void swapTwoValues(int &n1, int &n2){
+    int temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
+void swapTwoValues(float &n1, float &n2){
+    float temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
